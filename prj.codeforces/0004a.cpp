7b7d369ba9053a943f76c6ee9a45339d0b9baff8
CardlnaI <159645818+CardlnaI@users.noreply.github.com>
2025-11-14 11:25:00 +0300

    Create 0004a.cpp

---
+++ prj.codeforces/0004a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int a;
+    std::cin >> a;
+    std::string ans;
+
+    if (a % 2 or a < 4) { ans = "NO"; }
+    else { ans = "YES"; }
+    std::cout << ans;
+}
